3281264e11dcb36005260bda0c11a9399648aa5c
Zebulun Arendsee <arendsee@iastate.edu>
2020-03-25 13:11:40 -0500

    Mangle all standard functions

--- cpp_math.hpp
+++ cpp_math.hpp
@@ -55,4 +55,65 @@ std::vector<double> fibonacci(size_t n){
     return(xs);
 }
 
+double morloc_acos(double x){
+    return std::acos(x);
+}
+double morloc_asin(double x){
+    return std::asin(x);
+}
+double morloc_atan(double x){
+    return std::atan(x);
+}
+double morloc_atan2(double x, double y){
+    return std::atan2(x, y);
+}
+double morloc_ceil(double x){
+    return std::ceil(x);
+}
+double morloc_cos(double x){
+    return std::cos(x);
+}
+double morloc_cosh(double x){
+    return std::cosh(x);
+}
+double morloc_exp(double x){
+    return std::exp(x);
+}
+double morloc_fabs(double x){
+    return std::fabs(x);
+}
+double morloc_floor(double x){
+    return std::floor(x);
+}
+double morloc_fmod(double x, double y){
+    return std::fmod(x, y);
+}
+double morloc_pow(double x, double y){
+    return std::pow(x, y);
+}
+double morloc_ldexp(double x, int y){
+    return std::ldexp(x, y);
+}
+double morloc_log(double x){
+    return std::log(x);
+}
+double morloc_log10(double x){
+    return std::log10(x);
+}
+double morloc_sin(double x){
+    return std::sin(x);
+}
+double morloc_sinh(double x){
+    return std::sinh(x);
+}
+double morloc_sqrt(double x){
+    return std::sqrt(x);
+}
+double morloc_tan(double x){
+    return std::tan(x);
+}
+double morloc_tanh(double x){
+    return std::tanh(x);
+}
+
 #endif
